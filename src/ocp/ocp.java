5b340396c7fe4c951fd8256608fe8a3f50f62153
Fellipe Demoraes <fellipedemoraes@tamys-mbp.mynetworksettings.com>
2024-04-24 21:53:31 -0400

    ocp init

---
+++ src/ocp/ocp.java
@@ -0,0 +1,47 @@
+/**
+ * 
+ */
+package ocp;
+import pkg.cert;
+/**
+ * 
+ */
+public class ocp {
+
+	/**
+	 * 
+	 */
+	public ocp() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello");
+
+	}
+
+}
+
+
+class t extends cert {
+	public t() {
+		super();
+	}
+
+	@Override
+	public void x() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void y() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
---
+++ src/pkg/cert.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package pkg;
+
+/**
+ * 
+ */
+public abstract class cert {
+
+	/**
+	 * 
+	 */
+	public cert() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public abstract void x();
+	public abstract void y();
+
+}
